2424901f2305595291b9cf5b4a9b11f68388ff73
arijhy <alfrid.2021@gmail.com>
2026-09-15 14:41:23 +0300

    Program.cs

--- CheckedUncheckedBestPractices/Program.cs
+++ CheckedUncheckedBestPractices/Program.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace CheckedUncheckedBestPractices
 {
- class Program
+        class Program
         {
             static void Main()
             {
                 Console.WriteLine("SumChecked([1, 2, 3]) = " + SumCalculator.SumChecked(new[] { 1, 2, 3 }));
                 Console.WriteLine("SumChecked([int.MaxValue, 1]) = " + SumCalculator.SumChecked(new[] { int.MaxValue, 1 }));
 
-                Console.WriteLine("ParseOrZero_Bad(\"42\") = " + SumCalculator.ParseOrZero_Bad("42"));
-                Console.WriteLine("ParseOrZero_Good(\"42\") = " + SumCalculator.ParseOrZero_Good("42"));
-                Console.WriteLine("ParseOrZero_Good(\"abc\") = " + SumCalculator.ParseOrZero_Good("abc"));
+                Console.WriteLine("ParseOrZero(\"42\") = " + SumCalculator.ParseOrZero("42"));
+                Console.WriteLine("ParseOrZero(\"abc\") = " + SumCalculator.ParseOrZero("abc"));
 
                 Console.ReadKey();
             }
         }
     }
-
