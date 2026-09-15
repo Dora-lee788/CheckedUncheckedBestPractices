d9be2159177a1202911cda7c7dd31431a48e7b72
alfri <alfri@HUAWEI-DARIA>
2026-09-15 14:31:04 +0300

    Добавьте файлы проекта.

---
+++ CheckedUncheckedBestPractices/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedUncheckedBestPractices
+{
+ class Program
+        {
+            static void Main()
+            {
+                Console.WriteLine("SumChecked([1, 2, 3]) = " + SumCalculator.SumChecked(new[] { 1, 2, 3 }));
+                Console.WriteLine("SumChecked([int.MaxValue, 1]) = " + SumCalculator.SumChecked(new[] { int.MaxValue, 1 }));
+
+                Console.WriteLine("ParseOrZero_Bad(\"42\") = " + SumCalculator.ParseOrZero_Bad("42"));
+                Console.WriteLine("ParseOrZero_Good(\"42\") = " + SumCalculator.ParseOrZero_Good("42"));
+                Console.WriteLine("ParseOrZero_Good(\"abc\") = " + SumCalculator.ParseOrZero_Good("abc"));
+
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ CheckedUncheckedBestPractices/SumCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedUncheckedBestPractices
+{
+        static class SumCalculator
+        {
+            // Плохо
+            public static int ParseOrZero_Bad(string input)
+            {
+                try { return int.Parse(input); }
+                catch (Exception) { return 0; }
+            }
+
+            // Хорошо
+            public static int ParseOrZero_Good(string input)
+                => int.TryParse(input, out int result) ? result : 0;
+
+            public static int SumChecked(int[] numbers)
+            {
+                int sum = 0;
+                foreach (int number in numbers)
+                {
+                    try
+                    {
+                        sum = checked(sum + number);
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Сумма превышает диапазон int — возвращаем int.MaxValue.");
+                        return int.MaxValue;
+                    }
+                }
+                return sum;
+            }
+        }
+    }
+
