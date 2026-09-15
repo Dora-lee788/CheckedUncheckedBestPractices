88679531f2cb985b89c80dc58c251c7b6a7e006c
arijhy <alfrid.2021@gmail.com>
2026-09-15 14:43:11 +0300

    SumCalculator.cs

--- CheckedUncheckedBestPractices/SumCalculator.cs
+++ CheckedUncheckedBestPractices/SumCalculator.cs
@@ -8,17 +8,6 @@ namespace CheckedUncheckedBestPractices
 {
         static class SumCalculator
         {
-            // Плохо
-            public static int ParseOrZero_Bad(string input)
-            {
-                try { return int.Parse(input); }
-                catch (Exception) { return 0; }
-            }
-
-            // Хорошо
-            public static int ParseOrZero_Good(string input)
-                => int.TryParse(input, out int result) ? result : 0;
-
             public static int SumChecked(int[] numbers)
             {
                 int sum = 0;
@@ -36,6 +25,8 @@ namespace CheckedUncheckedBestPractices
                 }
                 return sum;
             }
+
+            public static int ParseOrZero(string input)
+                => int.TryParse(input, out int result) ? result : 0;
         }
     }
-
